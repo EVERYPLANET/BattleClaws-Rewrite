b89b47596f5bc52e0ad023c64c6d418e1538a389
LaurenMorgan93 <130102298+LaurenMorgan93@users.noreply.github.com>
2024-02-09 21:33:14 +0000

    Added ResultsCanvas to round scene

--- Assets/Scripts/FadeText.cs
+++ Assets/Scripts/FadeText.cs
@@ -6,16 +6,28 @@ using TMPro;
 public class FadeText : MonoBehaviour
 {
     private TextMeshProUGUI textObject;
+    private Image buttonPromptImage;
     [SerializeField]  private float speed;
 
     private void Start()
     {
         textObject = GetComponent<TextMeshProUGUI>();
+        buttonPromptImage = GetComponent<Image>();
+
+        if(buttonPromptImage != null)
+        {
+            StartCoroutine(flashImage());
+        }
+        
+       
     }
 
     private void Update()
     {
-        FadeInText();
+        if (textObject != null)
+        {
+            FadeInText();
+        }
     }
 
     public void FadeInText()
@@ -23,5 +35,18 @@ public class FadeText : MonoBehaviour
         {
             textObject.alpha = Mathf.Sin(Time.time * speed) * 0.5f + 0.5f;
         }
+    }
+
+    public IEnumerator flashImage()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        buttonPromptImage.color -= new Color(0f, 0f, 0f, 1f);
+
+        yield return new WaitForSeconds(0.5f);
+
+        buttonPromptImage.color += new Color(0f, 0f, 0f, 1f);
+
+        StartCoroutine(flashImage());
     }
 }
