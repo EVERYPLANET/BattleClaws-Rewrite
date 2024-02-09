6bd55e68efed10df475a8fab81fe5ca8f704a7ad
LaurenMorgan93 <130102298+LaurenMorgan93@users.noreply.github.com>
2024-02-09 18:34:13 +0000

    minor tweaks to PlayerSelect

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,18 +6,31 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private string destinationSceneName;
+    private string currentSceneName;
+    private bool isSplash = false;
+
+    private void Start()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+        if(currentSceneName == "Splash")
+        {
+            isSplash = true;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        
+        if(isSplash && Input.GetKeyDown(KeyCode.A))
         {
-            loadChosenScene();
+            StartCoroutine(loadChosenSceneWithDelay());
         }
     }
 
-    public void loadChosenScene() 
+    public IEnumerator loadChosenSceneWithDelay() 
     {
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(destinationSceneName);
     }
 }
