eaa6873db2f7cb0e3f90dfd3f22c71f74819f64e
LaurenMorgan93 <130102298+LaurenMorgan93@users.noreply.github.com>
2024-03-01 19:00:37 +0000

    Created Tomato Launcher and draw scene

---
+++ Assets/Scripts/TomatoBehaviour.cs
@@ -0,0 +1,42 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TomatoBehaviour : MonoBehaviour
+{
+    private float tomatoSpeed = 1f;
+    public GameObject SplatParticles;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * tomatoSpeed, ForceMode.Impulse);
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnDestroy()
+    {
+        Instantiate(SplatParticles, transform.position, transform.rotation); 
+    }
+
+
+   
+}
